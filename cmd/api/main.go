ea55b3f465dc3654487beeeead5b94f26d2a9cf2
Chris Armitage <dev@chrisarmitage.com>
2022-12-06 16:20:23 +0000

    Initial broker service

---
+++ cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8080"
+
+/**
+ * The "receiver"
+ */
+type Config struct {}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	// Define http server
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
