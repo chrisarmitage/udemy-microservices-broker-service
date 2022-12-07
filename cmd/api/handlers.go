11d27fe09e46379518e5f0ce30fbd061d91f62e0
Chris Armitage <dev@chrisarmitage.com>
2022-12-07 12:15:40 +0000

    Extract out JSON helper methods

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error bool `json:"error"`
-	Message string `json:"message"`
-	Data any `json:"data,omitempty"`
-}
-
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
-		Error: false,
-		Message: "Hit the broker",
+		Error:   false,
+		Message: "Hit the broker v2",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content_Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload)
 }
